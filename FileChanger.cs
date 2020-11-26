a2184a8846d0a97763a4975409cc15904966dd4d
stu124043 <stu124043@mail.uni-kiel.de>
2020-11-26 11:45:34 +0100

    Projektdateien hinzufügen.

---
+++ FileChanger.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Addressbook
+{
+    class FileChanger
+    {
+        internal string Path;
+        private List<List<string>> addressList = new List<List<string>>();
+
+        public FileChanger(string path)
+        {
+            Path = path;
+            onLoad();
+        }
+
+        /// <summary>
+        /// Opens and reads a file at the current path variable.
+        /// If no file exists, the method creates a new one.
+        /// </summary>
+        private void onLoad()
+        {
+            //reads in all the data in  the csv file
+            if (!File.Exists("addressbook.csv"))
+            {
+                File.Create(Path);
+                return;
+            }
+            TextFieldParser parser = new TextFieldParser(Path);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            string[] dataSnippets = parser.ReadFields();
+            List<string> tempEntry = new List<string>();
+
+            //prepares the raw data into Lists
+            if (dataSnippets == null) return;
+            foreach (string snippet in dataSnippets)
+            {
+                //the "New" keyword marks a new entry
+                if (snippet == "New")
+                {
+                    if (tempEntry.Count != 0)
+                    {
+                        addressList.Add(tempEntry);
+                    }
+                    tempEntry = new List<string>();
+                }
+                else
+                {
+                    tempEntry.Add(snippet);
+                }
+            }
+            addressList.Add(tempEntry);
+        }
+
+        /// <summary>
+        /// Searches the address book for a given key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>all addresses which contain an entry which contains the key</returns>
+        public List<List<string>> Search(string key)
+        {
+            List<List<string>> result = new List<List<string>>();
+            foreach (List<string> address in addressList)
+            {
+                foreach (string entry in address)
+                {
+                    if (entry.Contains(key))
+                    {
+                        result.Add(address);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Adds a new Entry
+        /// </summary>
+        /// <param name="newAddress"></param>
+        public void AddNewEntry(List<string> newAddress)
+        {
+            addressList.Add(newAddress);
+        }
+
+        /// <summary>
+        /// Changes a variable within an address.
+        /// DOES NOT change all entries in that address.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="oldKey">used to find the variable which needs replacement</param>
+        /// <param name="newKey"></param>
+        public bool UpdateEntry(string name, string oldKey, string newVariable)
+        {
+            bool updated = false;
+            foreach (List<string> address in addressList)
+            {
+                if (address[0].Contains(name))
+                {
+                    for (int i = 0; i < address.Count; ++i)
+                    {
+                        if (address[i].Contains(oldKey))
+                        {
+                            address[i] = newVariable;
+                            updated = true;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            return updated;
+        }
+
+        /// <summary>
+        /// Deletes an existing entry.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public bool DeleteEntry(string name)
+        {
+            bool isDeleted = false;
+            foreach (List<string> address in addressList)
+            {
+                if (address[0] == name)
+                {
+                    addressList.Remove(address);
+                    isDeleted = true;
+                    break;
+                }
+            }
+            return isDeleted;
+        }
+
+        /// <summary>
+        /// Prints the current addressList into the current path.
+        /// </summary>
+        public void PrintAddressBook()
+        {
+            string text = "";
+            foreach (List<string> address in addressList)
+            {
+                text += "New, ";
+                foreach (string entry in address)
+                {
+                    text += entry + ", ";
+                }
+            }
+            text = text.Substring(0, text.Length - 2);
+            System.IO.File.WriteAllText(Path, text);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Addressbook
+{
+    class Program
+    {
+        static FileChanger Changer;
+        static void Main(string[] args)
+        {
+            Changer = new FileChanger(System.AppDomain.CurrentDomain.BaseDirectory + "addressbook.csv");
+
+            Console.WriteLine("Welcome to your personal address book");
+            while (true)
+            {
+                string entry = getLine(
+                    "\nAdd a new entry\t\t\t(0)\n" +
+                    "Search for an entry\t\t(1)\n" +
+                    "Update an existing entry\t(2)\n" +
+                    "Delete an existing entry\t(3)\n" +
+                    "Save your changes and Exit\t(4)\n" +
+                    "Exit without Changes\t\t(Any Key)\n" +
+                    "\nPlease enter your action: "
+                    );
+
+                switch (entry)
+                {
+                    case "0":
+                        List<string> newEntry = new List<string>();
+                        newEntry.Add(getLine("Please enter a name: "));
+                        string input = "";
+                        while (input != "*")
+                        {
+                            input = getLine("Please enter an entry (or * to stop): ");
+                            if (input != "*") newEntry.Add(input);
+                        }
+                        Changer.AddNewEntry(newEntry);
+                        Console.WriteLine("We added a new entry for: " + newEntry[1] + "\n");
+                        break;
+                    case "1":
+                        string searchKey = getLine("Please enter a search key: ");
+                        List<List<string>> temp = Changer.Search(searchKey);
+                        Console.WriteLine("Search Results: ");
+                        if (temp.Count == 0)
+                        {
+                            Console.WriteLine("Nothing\n");
+                        }
+                        else
+                        {
+                            foreach (List<string> address in temp)
+                            {
+                                string str = "[ ";
+                                foreach (string entrie in address)
+                                {
+                                    str += entrie + ", ";
+                                }
+                                Console.WriteLine(str.Substring(0, str.Length - 2) + " ]");
+                            }
+                            Console.WriteLine("\n");
+                        }
+                        break;
+                    case "2":
+                        string[] updateKeys = new string[3];
+                        updateKeys[0] = getLine("Please enter the name of the person to update: ");
+                        updateKeys[1] = getLine("Please enter the entry you wish to update: ");
+                        updateKeys[2] = getLine("Please enter the new value: ");
+                        if (Changer.UpdateEntry(updateKeys[0], updateKeys[1], updateKeys[2]))
+                        {
+                            Console.WriteLine("\nUpdated the entry for " + updateKeys[0] + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSorry, the update failed. Please try a different key\n");
+                        }
+                        break;
+                    case "3":
+                        string deleteName = getLine("Please enter the name of the person whose entry you wish to delete: ");
+                        if (Changer.DeleteEntry(deleteName))
+                        {
+                            Console.WriteLine("\nDeleted the entry for " + deleteName + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSorry, we could not find the entry to delete\n");
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("\nYour addressbook is saved, goodbye.\n");
+                        Changer.PrintAddressBook();
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("\nThank you and have a nice day\n");
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads user input from the console and prepares it.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static string getLine(string input)
+        {
+            Console.WriteLine(input);
+            return Console.ReadLine().Trim();
+        }
+    }
+}
